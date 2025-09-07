6cf3245b6e187c92e14ed1aae60dfe4d600e5338
Chong Liu <chongliu2021@163.com>
2025-09-07 14:43:08 +0800

    Core API Tutorial Series - Adding a Manipulator Robot - Using the PickAndPlace Controller

--- Core_API_Tutorial_Series/3_Adding_a_Manipulator_Robot/hello_world.py
+++ Core_API_Tutorial_Series/3_Adding_a_Manipulator_Robot/hello_world.py
@@ -14,10 +14,11 @@
 # limitations under the License.
 
 from isaacsim.examples.interactive.base_sample import BaseSample
-
-# Can be used to create a new cube or to point to an already existing cube in stage.
+from isaacsim.robot.manipulators.examples.franka import Franka
 from isaacsim.core.api.objects import DynamicCuboid
+from isaacsim.robot.manipulators.examples.franka.controllers import PickPlaceController
 import numpy as np
+
 
 # Note: checkout the required tutorials at https://docs.omniverse.nvidia.com/app_isaacsim/app_isaacsim/overview.html
 
@@ -31,16 +32,16 @@ class HelloWorld(BaseSample):
         world = self.get_world()
         world.scene.add_default_ground_plane()
 
-        fancy_cube = world.scene.add(       # noqa: F841
+        franka = world.scene.add(Franka(prim_path="/World/Fancy_Franka", name="fancy_franka"))  # noqa: F841
+        world.scene.add(
             DynamicCuboid(
-                prim_path="/World/random_cube",     # The prim path of the cube in the USD stage
-                name="fancy_cube",                  # The unique name used to retrieve the object from the scene later on
-                position=np.array([0, 0, 1.0]),     # Using the current stage units which is in meters by default.
-                scale=np.array([0.5015, 0.5015, 0.5015]),     # most arguments accept mainly numpy arrays.
-                color=np.array([0, 0, 1.0]),        # RGB channels, going from 0-1
+                prim_path="/World/random_cube",
+                name="fancy_cube",
+                position=np.array([0.3, 0.3, 0.3]),
+                scale=np.array([0.0515, 0.0515, 0.0515]),
+                color=np.array([0, 0, 1.0]),
             )
         )
-
         return
 
     # Here we assign the class's variables
@@ -52,30 +53,47 @@ class HelloWorld(BaseSample):
     # many physical properties of the different objects
     async def setup_post_load(self):
         self._world = self.get_world()
-        self._cube = self._world.scene.get_object("fancy_cube")
-        # position, orientation = self._cube.get_world_pose()
-        # linear_velocity = self._cube.get_linear_velocity()
-        # # will be shown on terminal
-        # print("Cube position is : " + str(position))
-        # print("Cube's orientation is : " + str(orientation))
-        # print("Cube's linear velocity is : " + str(linear_velocity))
-        self._world.add_physics_callback("sim_step", callback_fn=self.print_cube_info)  # callback names have to be unique
+        self._franka = self._world.scene.get_object("fancy_franka")
+        self._fancy_cube = self._world.scene.get_object("fancy_cube")
+        self._controller = PickPlaceController(
+            name="pick_place_controller",
+            gripper=self._franka.gripper,
+            robot_articulation=self._franka,
+        )
+        self._world.add_physics_callback("sim_step", callback_fn=self.physics_step)
+        self._franka.gripper.set_joint_positions(self._franka.gripper.joint_opened_positions)
+        await self._world.play_async()
         return
-
-    # here we define the physics callback to be called before each physics step, all physics callbacks must take
-    # step_size as an argument
-    def print_cube_info(self, step_size):
-        position, orientation = self._cube.get_world_pose()
-        linear_velocity = self._cube.get_linear_velocity()
-        # will be shown on terminal
-        print("Cube position is : " + str(position))
-        print("Cube's orientation is : " + str(orientation))
-        print("Cube's linear velocity is : " + str(linear_velocity))
 
     async def setup_pre_reset(self):
         return
 
     async def setup_post_reset(self):
+        print("=" * 25, "setup_post_reset", "=" * 25)
+        self._controller.reset()
+        fancy_cube_random_pos = np.random.rand(3) * 0.3 + 0.2
+        print("fancy_cube_random_pos:", fancy_cube_random_pos)
+        print("franka position:", self._franka.get_world_pose())
+        self._fancy_cube.set_world_pose(position=fancy_cube_random_pos)
+        self._world.add_physics_callback("sim_step", callback_fn=self.physics_step)
+        self._franka.gripper.set_joint_positions(self._franka.gripper.joint_opened_positions)
+        await self._world.play_async()
+        return
+
+    def physics_step(self, step_size):
+        # print("current time: ", self._world.current_time)
+        cube_position, _ = self._fancy_cube.get_world_pose()
+        goal_position = np.array([-0.3, -0.3, 0.0515 / 2.0])
+        current_joint_positions = self._franka.get_joint_positions()
+        actions = self._controller.forward(
+            picking_position=cube_position,
+            placing_position=goal_position,
+            current_joint_positions=current_joint_positions,
+        )
+        self._franka.apply_action(actions)
+        # Comment out the pause to keep physics_step running continuously
+        if self._controller.is_done():
+            self._world.pause()
         return
 
     def world_cleanup(self):
