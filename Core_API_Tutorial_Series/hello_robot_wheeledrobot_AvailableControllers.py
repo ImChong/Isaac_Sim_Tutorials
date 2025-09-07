ac01bc422628b0c6ddf90949e9ade595091fa8cf
Chong Liu <chongliu2021@163.com>
2025-09-07 14:05:08 +0800

    Core API Tutorial Series - Adding a Controller

--- Core_API_Tutorial_Series/hello_robot_wheeledrobot_AvailableControllers.py
+++ Core_API_Tutorial_Series/hello_robot_wheeledrobot_AvailableControllers.py
@@ -15,7 +15,8 @@
 
 from isaacsim.examples.interactive.base_sample import BaseSample
 from isaacsim.robot.wheeled_robots.robots import WheeledRobot
-from isaacsim.core.utils.types import ArticulationAction
+from isaacsim.robot.wheeled_robots.controllers.wheel_base_pose_controller import WheelBasePoseController
+from isaacsim.robot.wheeled_robots.controllers.differential_controller import DifferentialController
 import numpy as np
 
 # Note: checkout the required tutorials at https://docs.omniverse.nvidia.com/app_isaacsim/app_isaacsim/overview.html
@@ -48,13 +49,18 @@ class HelloWorld(BaseSample):
         self._world = self.get_world()
         self._jetbot = self._world.scene.get_object("fancy_robot")
         self._world.add_physics_callback("sending_actions", callback_fn=self.send_robot_actions)
+        self._my_controller = WheelBasePoseController(name="cool_controller",
+                                                      open_loop_wheel_controller=DifferentialController(name="simple_control",
+                                                                                                        wheel_radius=0.03, wheel_base=0.1125),
+                                                      is_holonomic=False)
         return
 
     def send_robot_actions(self, step_size):
         print(self._world.current_time)
-        self._jetbot.apply_wheel_actions(ArticulationAction(joint_positions=None,
-                                                            joint_efforts=None,
-                                                            joint_velocities=5 * np.random.rand(2,)))
+        position, orientation = self._jetbot.get_world_pose()
+        self._jetbot.apply_action(self._my_controller.forward(start_position=position,
+                                                              start_orientation=orientation,
+                                                              goal_position=np.array([0.8, 0.8])))
         return
 
     async def setup_pre_reset(self):
