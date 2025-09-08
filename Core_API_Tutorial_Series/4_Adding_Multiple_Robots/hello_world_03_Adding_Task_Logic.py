6ae8bdb20b59a3a3d4e026da295b0dce4903f934
Chong Liu <chongliu2021@163.com>
2025-09-09 01:25:22 +0800

    Core API Tutorial Series - Adding Multiple Robots - Adding Task Logic

--- Core_API_Tutorial_Series/4_Adding_Multiple_Robots/hello_world_03_Adding_Task_Logic.py
+++ Core_API_Tutorial_Series/4_Adding_Multiple_Robots/hello_world_03_Adding_Task_Logic.py
@@ -4,6 +4,7 @@ from isaacsim.robot.wheeled_robots.robots import WheeledRobot
 from isaacsim.robot.wheeled_robots.controllers.wheel_base_pose_controller import WheelBasePoseController
 from isaacsim.robot.wheeled_robots.controllers.differential_controller import DifferentialController
 from isaacsim.core.api.tasks import BaseTask
+from isaacsim.core.utils.types import ArticulationAction
 import numpy as np
 
 
@@ -11,6 +12,7 @@ class RobotsPlaying(BaseTask):
     def __init__(self, name):
         super().__init__(name=name, offset=None)
         self._jetbot_goal_position = np.array([1.3, 0.3, 0])
+        self._task_event = 0
         self._pick_place_task = PickPlace(cube_initial_position=np.array([0.1, 0.3, 0.05]),
                                           target_position=np.array([0.7, -0.3, 0.0515 / 2.0]))
         return
@@ -39,6 +41,7 @@ class RobotsPlaying(BaseTask):
     def get_observations(self):
         current_jetbot_position, current_jetbot_orientation = self._jetbot.get_world_pose()
         observations = {
+            "task_event": self._task_event,
             self._jetbot.name: {
                 "position": current_jetbot_position,
                 "orientation": current_jetbot_orientation,
@@ -54,8 +57,25 @@ class RobotsPlaying(BaseTask):
         params_representation["franka_name"] = pick_place_params["robot_name"]
         return params_representation
 
+    def pre_step(self, control_index, simulation_time):
+        print("=" * 25, "task: pre_step", "=" * 25)
+        print("control_index:", control_index, "simulation_time:", simulation_time)
+        if self._task_event == 0:
+            current_jetbot_position, _ = self._jetbot.get_world_pose()
+            if np.mean(np.abs(current_jetbot_position[:2] - self._jetbot_goal_position[:2])) < 0.04:
+                self._task_event += 1
+                self._cube_arrive_step_index = control_index
+        elif self._task_event == 1:
+            # Jetbot has 200 time steps to back off from Franka
+            print("self._cube_arrive_step_index:", self._cube_arrive_step_index, "control_index:", control_index)
+            if control_index - self._cube_arrive_step_index == 200:
+                self._task_event += 1
+        return
+
     def post_reset(self):
         self._franka.gripper.set_joint_positions(self._franka.gripper.joint_opened_positions)
+        self._task_event = 0
+        return
 
 
 class HelloWorld(BaseSample):
@@ -87,10 +107,20 @@ class HelloWorld(BaseSample):
         return
 
     def physics_step(self, step_size):
+        print("=" * 20, "task: physics_step", "=" * 20)
         current_observations = self._world.get_observations()
-        self._jetbot.apply_action(
-            self._jetbot_controller.forward(
-                start_position=current_observations[self._jetbot.name]["position"],
-                start_orientation=current_observations[self._jetbot.name]["orientation"],
-                goal_position=current_observations[self._jetbot.name]["goal_position"]))
+        print("current_observations[task_event]:", current_observations["task_event"])
+        if current_observations["task_event"] == 0:
+            self._jetbot.apply_wheel_actions(
+                self._jetbot_controller.forward(
+                    start_position=current_observations[self._jetbot.name]["position"],
+                    start_orientation=current_observations[self._jetbot.name]["orientation"],
+                    goal_position=current_observations[self._jetbot.name]["goal_position"]))
+        elif current_observations["task_event"] == 1:
+            # Go backwards
+            self._jetbot.apply_wheel_actions(ArticulationAction(joint_velocities=[-8, -8]))
+        elif current_observations["task_event"] == 2:
+            # Apply zero velocity to override the velocity applied before.
+            # Note: target joint positions and target joint velocities will stay active unless changed
+            self._jetbot.apply_wheel_actions(ArticulationAction(joint_velocities=[0.0, 0.0]))
         return
